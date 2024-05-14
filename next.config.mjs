f2abfae5864f4db0a2a3ee12242f50afcf3e5fdb
Anna <>
2024-05-14 14:27:13 +0300

    set up git pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/test",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
